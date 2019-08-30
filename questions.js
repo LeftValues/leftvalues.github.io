27e34806e2bd8fa637f12b37b0a668c830ca170f
sparkz9502 <37208411+sparkz9502@users.noreply.github.com>
2019-08-30 00:13:43 -0500

    more questions

--- questions.js
+++ questions.js
@@ -1,4 +1,5 @@
 questions = [
+	//revolution vs reform
     {
         "question": "Reforming capitalist society to achieve better rights for workers is acceptable and desirable.",
         "effect": {
@@ -74,6 +75,87 @@ questions = [
         "effect": {
             "a": -5,
             "b": 0,
+            "c": 0,
+            "d": 0,
+			"e": 0
+        }
+    },
+	//scientific vs utopian
+	{
+		"question": "Material conditions and needs are the dominant drive behind socioeconomic changes.",
+        "effect": {
+            "a": 0,
+            "b": 10,
+            "c": 0,
+            "d": 0,
+			"e": 0
+        }
+    },
+	{
+		"question": "Socialism can only be fully achieved in developed industrialized societies.",
+        "effect": {
+            "a": 0,
+            "b": 5,
+            "c": 0,
+            "d": 0,
+			"e": 0
+        }
+    },
+	{
+		"question": "It is possible to peacefully convince the ruling class to conform to a socialist society.",
+        "effect": {
+            "a": 0,
+            "b": -10,
+            "c": 0,
+            "d": 0,
+			"e": 0
+        }
+    },
+	{
+		"question": "Capitalism will induce its own demise through contradictions that result in crisis.",
+        "effect": {
+            "a": 0,
+            "b": 5,
+            "c": 0,
+            "d": 0,
+			"e": 0
+        }
+    },
+	{
+		"question": "Class conflict is a force that has influenced much of history.",
+        "effect": {
+            "a": 0,
+            "b": 10,
+            "c": 0,
+            "d": 0,
+			"e": 0
+        }
+    },
+	{
+		"question": "The establishment of socialism is a gradual process, rather than an instant or rapid one.",
+        "effect": {
+            "a": 0,
+            "b": 5,
+            "c": 0,
+            "d": 0,
+			"e": 0
+        }
+    },
+	{
+		"question": "Society is chiefly driven by individuals and ideas.",
+        "effect": {
+            "a": 0,
+            "b": -10,
+            "c": 0,
+            "d": 0,
+			"e": 0
+        }
+    },
+	{
+		"question": "Total economic and social equality is possible to achieve.",
+        "effect": {
+            "a": 0,
+            "b": -10,
             "c": 0,
             "d": 0,
 			"e": 0
