e1479774e9bc94c9ca1d1b48883550e27046853f
sparkz9502 <37208411+sparkz9502@users.noreply.github.com>
2019-08-29 23:00:55 -0500

    start of questions

--- questions.js
+++ questions.js
@@ -1,632 +1,82 @@
 questions = [
     {
-        "question": "Oppression by corporations is more of a concern than oppression by governments.",
-        "effect": {
-            "a": 10,
-            "b": 0,
-            "c": 0,
-            "d": 0
-        }
-    },
-    {
-        "question": "It is necessary for the government to intervene in the aomy to protect consumers.",
-        "effect": {
-            "a": 10,
-            "b": 0,
-            "c": 0,
-            "d": 0
-        }
-    },
-    {
-        "question": "The freer the markets, the freer the people.",
-        "effect": {
-            "a": -10,
-            "b": 0,
-            "c": 0,
-            "d": 0
-        }
-    },
-    {
-        "question": "It is better to maintain a balanced budget than to ensure welfare for all citizens.",
-        "effect": {
-            "a": -10,
-            "b": 0,
-            "c": 0,
-            "d": 0
-        }
-    },
-    {
-        "question": "Publicly-funded research is more beneficial to the people than leaving it to the market.",
-        "effect": {
-            "a": 10,
-            "b": 0,
-            "c": 0,
-            "d": 10
-        }
-    },
-    {
-        "question": "International trade is beneficial.",
+        "question": "Reforming capitalist society to achieve better rights for workers is acceptable and desirable.",
         "effect": {
             "a": -5,
             "b": 0,
-            "c": 10,
-            "d": 0
+            "c": 0,
+            "d": 0,
+			"e": 0
         }
-    },
-    {
-        "question": "From each according to his ability, to each according to his needs.",
+	},
+	{
+		"question": "Revolution is the only way of achieving a socialist society.",
         "effect": {
             "a": 10,
             "b": 0,
             "c": 0,
-            "d": 5
-        }
-    },
-    {
-        "question": "It would be best if social programs were abolished in favor of private charity.",
+            "d": 0,
+			"e": 0
+        },
+	},
+	{
+		"question": "The negative consequences of a revolution outweigh the advantages.",
         "effect": {
             "a": -10,
             "b": 0,
             "c": 0,
-            "d": -5
+            "d": 0,
+			"e": 0
         }
-    },
-    {
-        "question": "Taxes should be increased on the rich to provide for the poor.",
-        "effect": {
-            "a": 10,
-            "b": 0,
-            "c": 0,
-            "d": 0
-        }
-    },
-    {
-        "question": "Inheritance is a legitimate form of wealth.",
+	},
+	{
+		"question": "Liberal democracy is a viable way of achieving a socialist society.",
         "effect": {
             "a": -10,
             "b": 0,
             "c": 0,
-            "d": -5
+            "d": 0,
+			"e": 0
         }
-    },
-    {
-        "question": "Public utilities like roads and electricity should be publicly owned.",
-        "effect": {
-            "a": 10,
-            "b": 0,
-            "c": 0,
-            "d": 0
-        }
-    },
-    {
-        "question": "Excessive government intervention is a threat to the aomy.",
-        "effect": {
-            "a": -10,
-            "b": 0,
-            "c": 0,
-            "d": 0
-        }
-    },
-    {
-        "question": "Those with a greater ability to pay should receive better healthcare.",
-        "effect": {
-            "a": -10,
-            "b": 0,
-            "c": 0,
-            "d": 0
-        }
-    },
-    {
-        "question": "Quality education is a right of all people.",
-        "effect": {
-            "a": 10,
-            "b": 0,
-            "c": 0,
-            "d": 10
-        }
-    },
-    {
-        "question": "The means of production should belong to the workers who use them.",
-        "effect": {
-            "a": 10,
-            "b": 0,
-            "c": 0,
-            "d": 0
-        }
-    },
-    {
-        "question": "The United Nations should be abolished.",
-        "effect": {
-            "a": 0,
-            "b": -10,
-            "c": -5,
-            "d": 0
-        }
-    },
-    {
-        "question": "Military action by our nation is often necessary to protect it.",
-        "effect": {
-            "a": 0,
-            "b": -10,
-            "c": -10,
-            "d": 0
-        }
-    },
-    {
-        "question": "I support regional unions, such as the European Union.",
-        "effect": {
-            "a": -5,
-            "b": 10,
-            "c": 10,
-            "d": 5
-        }
-    },
-    {
-        "question": "It is important to maintain our national sovereignty.",
-        "effect": {
-            "a": 0,
-            "b": -10,
-            "c": -5,
-            "d": 0
-        }
-    },
-    {
-        "question": "A united world government would be beneficial to mankind.",
-        "effect": {
-            "a": 0,
-            "b": 10,
-            "c": 0,
-            "d": 0
-        }
-    },
-    {
-        "question": "It is more important to retain peaceful relations than to further our strength.",
-        "effect": {
-            "a": 0,
-            "b": 10,
-            "c": 0,
-            "d": 0
-        }
-    },
-    {
-        "question": "Wars do not need to be justified to other countries.",
-        "effect": {
-            "a": 0,
-            "b": -10,
-            "c": -10,
-            "d": 0
-        }
-    },
-    {
-        "question": "Military spending is a waste of money.",
-        "effect": {
-            "a": 0,
-            "b": 10,
-            "c": 10,
-            "d": 0
-        }
-    },
-    {
-        "question": "International aid is a waste of money.",
-        "effect": {
-            "a": -5,
-            "b": -10,
-            "c": 0,
-            "d": 0
-        }
-    },
-    {
-        "question": "My nation is great.",
-        "effect": {
-            "a": 0,
-            "b": -10,
-            "c": 0,
-            "d": 0
-        }
-    },
-    {
-        "question": "Research should be conducted on an international scale.",
-        "effect": {
-            "a": 0,
-            "b": 10,
-            "c": 0,
-            "d": 10
-        }
-    },
-    {
-        "question": "Governments should be accountable to the international community.",
-        "effect": {
-            "a": 0,
-            "b": 10,
-            "c": 5,
-            "d": 0
-        }
-    },
-    {
-        "question": "Even when protesting an authoritarian government, violence is not acceptable.",
-        "effect": {
-            "a": 0,
-            "b": 5,
-            "c": -5,
-            "d": 0
-        }
-    },
-    {
-        "question": "My religious values should be spread as much as possible.",
-        "effect": {
-            "a": 0,
-            "b": -5,
-            "c": -10,
-            "d": -10
-        }
-    },
-    {
-        "question": "Our nation's values should be spread as much as possible.",
-        "effect": {
-            "a": 0,
-            "b": -10,
-            "c": -10,
-            "d": 0
-        }
-    },
-    {
-        "question": "It is very important to maintain law and order.",
-        "effect": {
-            "a": 0,
-            "b": -5,
-            "c": -10,
-            "d": -5
-        }
-    },
-    {
-        "question": "The general populace makes poor decisions.",
-        "effect": {
-            "a": 0,
-            "b": 0,
-            "c": -10,
-            "d": 0
-        }
-    },
-    {
-        "question": "Victimless crimes (such as drug use) should not be crimes at all.",
-        "effect": {
-            "a": 0,
-            "b": 0,
-            "c": 10,
-            "d": 0
-        }
-    },
-    {
-        "question": "The sacrifice of some civil liberties is necessary to protect us from acts of terrorism.",
-        "effect": {
-            "a": 0,
-            "b": 0,
-            "c": -10,
-            "d": 0
-        }
-    },
-    {
-        "question": "Government surveillance is necessary in the modern world.",
-        "effect": {
-            "a": 0,
-            "b": 0,
-            "c": -10,
-            "d": 0
-        }
-    },
-    {
-        "question": "The very existence of the state is a threat to our liberty.",
-        "effect": {
-            "a": 0,
-            "b": 0,
-            "c": 10,
-            "d": 0
-        }
-    },
-    {
-        "question": "Regardless of political opinions, it is important to side with your country.",
-        "effect": {
-            "a": 0,
-            "b": -10,
-            "c": -10,
-            "d": -5
-        }
-    },
-    {
-        "question": "No authority should be left unquestioned.",
-        "effect": {
-            "a": 0,
-            "b": 0,
-            "c": 10,
-            "d": 5
-        }
-    },
-    {
-        "question": "A hierarchical state is best.",
-        "effect": {
-            "a": 0,
-            "b": 0,
-            "c": -10,
-            "d": 0
-        }
-    },
-    {
-        "question": "It is important that the government follows the majority opinion, even if it is wrong.",
-        "effect": {
-            "a": 0,
-            "b": 0,
-            "c": 10,
-            "d": 0
-        }
-    },
-    {
-        "question": "The stronger the leadership, the better.",
-        "effect": {
-            "a": 0,
-            "b": -10,
-            "c": -10,
-            "d": 0
-        }
-    },
-    {
-        "question": "Democracy is more than a decision-making process.",
-        "effect": {
-            "a": 0,
-            "b": 0,
-            "c": 10,
-            "d": 0
-        }
-    },
-    {
-        "question": "Environmental regulations are essential.",
+	},
+	{
+		"question": "A socialist revolution is made inevitable by the conditions of capitalism.",
         "effect": {
             "a": 5,
             "b": 0,
             "c": 0,
-            "d": 10
+            "d": 0,
+			"e": 0
         }
-    },
-    {
-        "question": "A better world will come from automation, science, and technology.",
+	},
+	{
+		"question": "Workplace democracy within capitalism is an acceptable alternative to a complete socialization of the economy.",
         "effect": {
-            "a": 0,
+            "a": -10,
             "b": 0,
             "c": 0,
-            "d": 10
+            "d": 0,
+			"e": 0
         }
-    },
-    {
-        "question": "Children should be educated in religious or traditional values.",
-        "effect": {
-            "a": 0,
-            "b": 0,
-            "c": -10,
-            "d": -10
-        }
-    },
-    {
-        "question": "Traditions are of no value on their own.",
-        "effect": {
-            "a": 0,
-            "b": 0,
-            "c": 0,
-            "d": 10
-        }
-    },
-    {
-        "question": "Religion should play a role in government.",
-        "effect": {
-            "a": 0,
-            "b": 0,
-            "c": -10,
-            "d": -10
-        }
-    },
-    {
-        "question": "Churches should be taxed the same way other institutions are taxed.",
+	},
+	{
+		"question": "Revolutionary violence is acceptable as long as the final outcome is positive.",
         "effect": {
             "a": 5,
             "b": 0,
             "c": 0,
-            "d": 10
+            "d": 0,
+			"e": 0
         }
-    },
-    {
-        "question": "Climate change is currently one of the greatest threats to our way of life.",
+	},
+	{
+		"question": "Redistributing wealth away from the wealthy through taxes is a viable way of defeating inequality.",
         "effect": {
-            "a": 0,
+            "a": -5,
             "b": 0,
             "c": 0,
-            "d": 10
-        }
-    },
-    {
-        "question": "It is important that we work as a united world to combat climate change.",
-        "effect": {
-            "a": 0,
-            "b": 10,
-            "c": 0,
-            "d": 10
-        }
-    },
-    {
-        "question": "Society was better many years ago than it is now.",
-        "effect": {
-            "a": 0,
-            "b": 0,
-            "c": 0,
-            "d": -10
-        }
-    },
-    {
-        "question": "It is important that we maintain the traditions of our past.",
-        "effect": {
-            "a": 0,
-            "b": 0,
-            "c": 0,
-            "d": -10
-        }
-    },
-    {
-        "question": "It is important that we think in the long term, beyond our lifespans.",
-        "effect": {
-            "a": 0,
-            "b": 0,
-            "c": 0,
-            "d": 10
-        }
-    },
-    {
-        "question": "Reason is more important than maintaining our culture.",
-        "effect": {
-            "a": 0,
-            "b": 0,
-            "c": 0,
-            "d": 10
-        }
-    },
-    {
-        "question": "Drug use should be legalized or decriminalized.",
-        "effect": {
-            "a": 0,
-            "b": 0,
-            "c": 10,
-            "d": 2
-        }
-    },
-    {
-        "question": "Same-sex marriage should be legal.",
-        "effect": {
-            "a": 0,
-            "b": 0,
-            "c": 10,
-            "d": 10
-        }
-    },
-    {
-        "question": "No cultures are superior to others.",
-        "effect": {
-            "a": 0,
-            "b": 10,
-            "c": 5,
-            "d": 10
-        }
-    },
-    {
-        "question": "Sex outside marriage is immoral.",
-        "effect": {
-            "a": 0,
-            "b": 0,
-            "c": -5,
-            "d": -10
-        }
-    },
-    {
-        "question": "If we accept migrants at all, it is important that they assimilate into our culture.",
-        "effect": {
-            "a": 0,
-            "b": 0,
-            "c": -5,
-            "d": -10
-        }
-    },
-    {
-        "question": "Abortion should be prohibited in most or all cases.",
-        "effect": {
-            "a": 0,
-            "b": 0,
-            "c": -10,
-            "d": -10
-        }
-    },
-    {
-        "question": "Gun ownership should be prohibited for those without a valid reason.",
-        "effect": {
-            "a": 0,
-            "b": 10,
-            "c": -10,
-            "d": 0
-        }
-    },
-    {
-        "question": "I support single-payer, universal healthcare.",
-        "effect": {
-            "a": 10,
-            "b": 0,
-            "c": 0,
-            "d": 0
-        }
-    },
-    {
-        "question": "Prostitution should be illegal.",
-        "effect": {
-            "a": 0,
-            "b": 0,
-            "c": -10,
-            "d": -10
-        }
-    },
-    {
-        "question": "Maintaining family values is essential.",
-        "effect": {
-            "a": 0,
-            "b": 0,
-            "c": 0,
-            "d": -10
-        }
-    },
-    {
-        "question": "To chase progress at all costs is dangerous.",
-        "effect": {
-            "a": 0,
-            "b": 0,
-            "c": 0,
-            "d": -10
-        }
-    },
-    {
-        "question": "Genetic modification is a force for good, even on humans.",
-        "effect": {
-            "a": 0,
-            "b": 0,
-            "c": 0,
-            "d": 10
-        }
-    },
-    {
-        "question": "We should open our borders to immigration.",
-        "effect": {
-            "a": 0,
-            "b": 10,
-            "c": 10,
-            "d": 0
-        }
-    },
-    {
-        "question": "Governments should be as concerned about foreign citizens as they are about those within their borders.",
-        "effect": {
-            "a": 10,
-            "b": 10,
-            "c": 0,
-            "d": 0
-        }
-    },
-    {
-        "question": "All people - regardless of factors like culture or sexuality - should be treated equally.",
-        "effect": {
-            "a": 10,
-            "b": 10,
-            "c": 10,
-            "d": 10
-        }
-    },
-    {
-        "question": "It is important that we further my group's goals above all others.",
-        "effect": {
-            "a": -10,
-            "b": -10,
-            "c": -10,
-            "d": -10
+            "d": 0,
+			"e": 0
         }
     }
 ];
