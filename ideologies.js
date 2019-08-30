e5d7c9819edce59da3cd6220318d9af800712512
sparkz9502 <37208411+sparkz9502@users.noreply.github.com>
2019-08-30 03:51:51 -0500

    ideologies

--- ideologies.js
+++ ideologies.js
@@ -1,13 +1,90 @@
 ideologies = [
-    {
-        "name": "Temp",
+	{
+        "name": "Marxism-Leninism",
         "stats": {
-            "a": 50,
-            "b": 50,
+            "a": 70,
+            "b": 80,
+            "c": 90,
+            "d": 40,
+			"e": 100,
+			"f": 90
+        }
+    },
+    {
+        "name": "Orthodox Marxism",
+        "stats": {
+            "a": 80,
+            "b": 90,
+            "c": 70,
+            "d": 80,
+			"e": 70,
+			"f": 60
+        }
+    },
+	{
+        "name": "Eco-Marxism",
+        "stats": {
+            "a": 80,
+            "b": 90,
+            "c": 70,
+            "d": 80,
+			"e": 70,
+			"f": 20
+        }
+    },
+	{
+        "name": "Left Communism",
+        "stats": {
+            "a": 100,
+            "b": 60,
             "c": 50,
-            "d": 50,
-			"e": 50,
+            "d": 100,
+			"e": 40,
 			"f": 50
+        }
+    },
+	{
+        "name": "Anarcho-Communism",
+        "stats": {
+            "a": 90,
+            "b": 50,
+            "c": 20,
+            "d": 100,
+			"e": 10,
+			"f": 60
+        }
+    },
+	{
+        "name": "Eco-Anarchism",
+        "stats": {
+            "a": 90,
+            "b": 50,
+            "c": 20,
+            "d": 100,
+			"e": 10,
+			"f": 20
+        }
+    },
+	{
+        "name": "Utopian Socialism",
+        "stats": {
+            "a": 30,
+            "b": 20,
+            "c": 40,
+            "d": 20,
+			"e": 50,
+			"f": 40
+        }
+    },
+	{
+        "name": "Democratic Socialism",
+        "stats": {
+            "a": 20,
+            "b": 30,
+            "c": 30,
+            "d": 60,
+			"e": 60,
+			"f": 30
         }
     }
 ];
