68b713e4bccd5e17b7c724476908ed8455cbb10a
sparkz9502 <37208411+sparkz9502@users.noreply.github.com>
2019-08-31 05:17:38 -0500

    ideology descs

--- ideologies.js
+++ ideologies.js
@@ -1,6 +1,7 @@
 ideologies = [
 	{
         "name": "Marxism-Leninism",
+		"desc": "Marxism-Leninism is a form of Marxism that was forged in the 20th century in the Soviet Union. Marxist-Leninists heavily favor the use of a communist political party as the platform for both achieving revolution and establishing socialism. Many Marxist-Leninists are more nationalistic and patriotic than many other Marxists, and may favor industrial progress over environmental goals.",
         "stats": {
             "a": 70,
             "b": 80,
@@ -13,6 +14,7 @@ ideologies = [
     },
     {
         "name": "Orthodox Marxism",
+		"desc": "Orthodox Marxism is a form of Marxism that adopts views that conform to Marxist theory as it was originally written, particularily on the topic of dialectical materialism. Orthodox Marxists are highly revolutionary and internationalist and hold that nonindustrial societies are incapable of achieving socialism.",
         "stats": {
             "a": 80,
             "b": 90,
@@ -25,6 +27,7 @@ ideologies = [
     },
 	{
         "name": "Eco-Marxism",
+		"desc": "Eco-Marxism is a form of Orthodox Marxism more commonly found in the modern day that places a particular emphasis on environmental issues. Eco-Marxists generally favor central economic planning as a way of combating climate issues.",
         "stats": {
             "a": 80,
             "b": 90,
@@ -37,6 +40,7 @@ ideologies = [
     },
 	{
         "name": "Centrist Marxism",
+		"desc": "Centrist Marxism is a form of Marxism that adopts Marxist views on society and the economy while also refraining from taking a definitive position on revolution and reformism. Many Centrist Marxists may also be more nationalistic than other Marxists.",
         "stats": {
             "a": 40,
             "b": 80,
@@ -49,6 +53,7 @@ ideologies = [
     },
 	{
         "name": "Council Communism",
+		"desc": "Council Communism is a form of Marxism that advocates for workers councils being the core foundation of a socialist society. Council Communists generally oppose the existence of strong influence from political parties within the socialist political system, favoring council democracy.",
         "stats": {
             "a": 90,
             "b": 70,
@@ -61,6 +66,7 @@ ideologies = [
     },
 	{
         "name": "Left Communism",
+		"desc": "Left Communism is a form of Marxism that leans towards so called ultra-left positions. These include unconditional support for revolution and internationalism, a rejection of unions and participation in liberal democracy, and a rejection of national liberation and self-determination.",
         "stats": {
             "a": 100,
             "b": 60,
@@ -73,6 +79,7 @@ ideologies = [
     },
 	{
         "name": "Anarcho-Communism",
+		"desc": "Anarcho-Communism is a form of anarchism that advocates for the abolition of capitalism and the state, as well as the collective ownership of the means of production. Anarcho-Communists support a decentralized economy and government, with Anarcho-Syndicalism being a common way of organizing such a society.",
         "stats": {
             "a": 90,
             "b": 50,
@@ -85,8 +92,9 @@ ideologies = [
     },
 	{
         "name": "Eco-Anarchism",
+		"desc": "Eco-Anarchism, or Green Anarchism, is a form of anarchism that places a particular emphasis on environmental issues. It is often linked to more distinct ideologies such as Anarcho-Syndicalism. Eco-Anarchists are generally revolutionary and support using a decentralized egalitarian economy to achieve environmental goals.",
         "stats": {
-            "a": 90,
+            "a": 80,
             "b": 50,
             "c": 20,
             "d": 100,
@@ -97,6 +105,7 @@ ideologies = [
     },
 	{
         "name": "Market Anarchism",
+		"desc": "Market Anarchism is a form of anarchism that mixes anarchist goals such as the abolition of hierarchy, the state and capitalism with heightened individualism and a moderate reformist approach. This should not be confused with Anarcho-Capitalism.",
         "stats": {
             "a": 20,
             "b": 40,
@@ -109,6 +118,7 @@ ideologies = [
     },
 	{
         "name": "Utopian Socialism",
+		"desc": "Utopian Socialism is a form of pre-Marxist socialism that belives highly in an egalitarian, moralistic and idealistic foundation for a socialist society. Utopian Socialists generally reject violent revolution and often believe the ruling class can be convinced to adopt socialism.",
         "stats": {
             "a": 30,
             "b": 20,
@@ -121,6 +131,7 @@ ideologies = [
     },
 	{
         "name": "Democratic Socialism",
+		"desc": "Democratic Socialism is a form of socialism that seeks to utilize liberal democracy as a means to achieve a socialist economy and society. Democratic Socialist reject revolution and a centrally planned economy, instead supporting moderate social ownership in the form of publicly owned utilities and democratic workplace self-management.",
         "stats": {
             "a": 20,
             "b": 30,
@@ -133,6 +144,7 @@ ideologies = [
     },
 	{
         "name": "Social Democracy",
+		"desc": "Social Democracy is a centre-left ideology that advocates for mixing left-leaning values such as social welfare and corporate regulation with capitalism and liberal democracy in the form of a mixed economy. Many modern Social Democrats favor Keynesian economics.",
         "stats": {
             "a": 10,
             "b": 30,
