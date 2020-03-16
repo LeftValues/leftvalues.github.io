3894630bb6804ff85b4bd4e2d16680ed3f827011
Sybil J <39376134+LastWitchOfIllyrith@users.noreply.github.com>
2020-03-16 17:12:51 +0100

    sh translation part 1

--- i18n.js
+++ i18n.js
@@ -37,7 +37,11 @@ const languages = [
     {
         "name": "简体中文",
         "code": "cn"
-    }
+    },
+	{
+        "name": "srpskohrvatski",
+        "code": "sh"
+    },
 ]
 const i18n = {
     getString(name, arguments=null) {
@@ -58,56 +62,64 @@ const i18n = {
         "de": "Laden…",
         "ko": "로딩중...",
         "ru": "Загрузка...",
-        "cn": "载入中..."
+        "cn": "载入中...",
+		"sh": "Učitavanje..."
     },
     "quiz-strongly-agree": {
         "en": "Strongly Agree",
         "de": "Stimme voll zu",
         "ko": "매우 동의",
         "ru": "Полностью согласен",
-        "cn": "强烈同意"
+        "cn": "强烈同意",
+		"sh": "Potpuno Se Slažem"
     },
     "quiz-agree": {
         "en": "Agree",
         "de": "Stimme zu",
         "ko": "동의",
         "ru": "Скорее согласен",
-        "cn": "同意"
+        "cn": "同意",
+		"sh": "Donekle Se Slažem"
     },
     "quiz-neutral": {
         "en": "Neutral/Unsure",
         "de": "Neutral/Unsicher",
         "ko": "중립/잘 모름",
         "ru": "Не знаю/Не уверен",
-        "cn": "中立/不确定"
+        "cn": "中立/不确定",
+		"sh": "Nemam Mišljenje/Ne znam"
     },
     "quiz-disagree": {
         "en": "Disagree",
         "de": "Stimme nicht zu",
         "ko": "동의하지 않음",
         "ru": "Скорее не согласен",
-        "cn": "反对"
+        "cn": "反对",
+		"sh": "Donekle Se Ne Slažem"
     },
     "quiz-strongly-disagree": {
         "en": "Strongly Disagree",
         "de": "Stimme überhaupt nicht zu",
         "ko": "결코 동의하지 않음",
         "ru": "Полностью не согласен",
-        "cn": "强烈反对"
+        "cn": "强烈反对",
+		"sh": "Uopšte Se Ne Slažem"
     },
     "quiz-back": {
         "de": "back",
         "de": "Zurück",
         "ko": "뒤로",
         "ru": "Назад",
-        "cn": "返回"
+        "cn": "返回",
+		"sh": "Nazad"
     },
     "quiz-question-of": {
         en(qn, questions) {return `Question ${qn + 1} of ${questions.length}`},
         de(qn, questions) {return `Frage ${qn +1} von ${questions.length}`},
         ko(qn, questions) {return `${questions.length}개 질문 중 ${qn +1} 번째`},
         ru(qn, questions) {return `Вопрос ${qn + 1} из ${questions.length}`},
-        cn(qn, questions) {return `第 ${qn + 1} 题，共 ${questions.length} 题`}
+        cn(qn, questions) {return `第 ${qn + 1} 题，共 ${questions.length} 题`},
+		sh(qn, questions) {return `Pitanje ${qn + 1} od ${questions.length}`}
     },
     // instructions.html
     "inst-h2": {
@@ -115,28 +127,32 @@ const i18n = {
         "de": "Instruktionen",
         "ko": "소개",
         "ru": "Инструкция",
-        "cn": "说明"
+        "cn": "说明",
+		"sh": "Uputstva"
     },
     "inst-p": {
         "en": "You will be presented with a series of statements. For each one, click the button with your opinion on it.",
         "de": "Sie erhalten eine Reihe von Stellungnahmen. Klicken Sie jeweils auf die Schaltfläche mit Ihrer Meinung dazu.",
         "ko": "당신은 일련의 질문들을 받게 될 것입니다. 해당되는 답변을 클릭하세요.",
         "ru": "Вам будет предложен ряд утверждений. Для каждого нажмите на кнопку с вашим мнением о нём.",
-        "cn": "你会看到一系列观点。对于每个观点，请选择你的看法。"
+        "cn": "你会看到一系列观点。对于每个观点，请选择你的看法。",
+		"sh": "Biće vam predstavljen niz izjava. Za svaku, kliknite dugme na kojem je vaše mišljenje."
     },
     "inst-gotit": {
         "en": "Got it!",
         "de": "Verstanden",
         "ko": "알겠습니다",
         "ru": "Понял!",
-        "cn": "知道了！"
+        "cn": "知道了！",
+		"sh": "Razumijem!"
     },
     "inst-nvm": {
         "en": "Wait, nevermind!",
         "de": "Warte, vergiss es!",
         "ko": "그만둘래요.",
         "ru": "Вернуться",
-        "cn": "等等，还是算了！"
+        "cn": "等等，还是算了！",
+		"sh": "Čekaj, nema veze!"
     },
     // results.html
     "result-h1": {
@@ -144,105 +160,120 @@ const i18n = {
         "de": "Ergebnisse",
         "ko": "결과",
         "ru": "Результаты",
-        "cn": "结果"
+        "cn": "结果",
+		"sh": "Rezultati"
     },
     "result-url": {
         "en": "<br>You can send these results by copying and pasting the URL at the top of the page or using the image below.",
         "de": "<br>Sie können diese Ergebnisse senden, indem Sie die URL oben auf der Seite kopieren und einfügen oder das folgende Bild verwenden.",
         "ko": "<br>당신은 상단의 URL을 복사하거나 아래 이미지를 이용해 설문 결과를 공유할 수 있습니다.",
         "ru": "<br>Вы можете отправить эти результаты, скопировав и вставив URL в верхней части страницы или используя изображение ниже.",
-        "cn": "<br>你可以复制粘贴页面顶端的URL或是使用下方的图片来分享结果。"
+        "cn": "<br>你可以复制粘贴页面顶端的URL或是使用下方的图片来分享结果。",
+		"sh": "<br>Možete da pošaljete ove rezultate kopirajući URL na vrhu stranice ili koristeći sliku ispod."
     },
     "result-h2-match": {
         "en": "Closest Match: <span class='weight-300' id='ideology-label'> </span>",
         "de": "Höchste Übereinstimmung: <span class='weight-300' id='ideology-label'> </span>",
         "ko": `가장 일치하는 성향: <span class="weight-300" id="ideology-label"> </span>`,
         "ru": "Ближайшее совпадение: <span class='weight-300' id='ideology-label'> </span>",
-        "cn": "最接近的匹配: <span class='weight-300' id='ideology-label'> </span>"
+        "cn": "最接近的匹配: <span class='weight-300' id='ideology-label'> </span>",
+		"sh": "Najbliže podudaranje: <span class='weight-300' id='ideology-label'> </span>"
     },
     "result-h2-next-matches": {
         "en": "Next closest matches",
         "de": "Nächst höchste Überstimmungen",
         "ko": "다음으로 일치하는 성향",
         "ru": "Следующие ближайшие совпадения",
-        "cn": "接下来最接近的匹配"
+        "cn": "接下来最接近的匹配",
+		"sh": "Sledeća najbliža podudaranja"
     },
     "next-matches-percent": {
         "en": "With your closest match as 100% and farthest as 0%, here is how closely you matched the other ideologies.",
         "de": "Hier sind die Ideologien mit denen du am meisten übereinstimmst, wo volle Übereinstimmung 100% ist und keine 0%.",
         "ko": "당신과 가장 일치하는 성향을 100%로, 가장 일치하지 않는 성향을 0%로 둘 때, 아래 사상들은 당신의 성향에 가까운 다른 다른 사상들 입니다.",
         "ru": "С вашим самым близким соответствием в 100% и самым дальним на 0%, вот как близко вы подходите к другим идеологиям.",
-        "cn": "这是你与其他意识形态之间的匹配程度，最接近的为100%，最远的为0%。"
+        "cn": "这是你与其他意识形态之间的匹配程度，最接近的为100%，最远的为0%。",
+		"sh": "Sa vašom najbližom na 100% i najdaljom na 0%, evo koliko su vam bliske ostale ideologije."
     },
     "result-h2-score": {
         "en": "I don't like my scores!",
         "de": "Ich mag meine Ergebnisse nicht",
         "ko": "제 결과가 마음에 들지 않습니다.",
         "ru": "Мне не нравятся мои результаты!",
-        "cn": "我不喜欢我的分数！"
+        "cn": "我不喜欢我的分数！",
+		"sh": "Ne sviđaju mi se moji rezultati!"
     },
     "result-issues": {
         "en": "Please remember that you are not intended to get a 100% score in any of the categories. The point of the quiz is to challenge your views. If you have any suggestions or constructive criticism please fill out this <a href='https://forms.gle/6WZYMb5GXkkDLhxr5'>short form</a> or open an issue on the <a href='https://github.com/LeftValues/leftvalues.github.io'>GitHub Page</a>.",
         "de": "Bitte denken Sie daran, dass Sie in keiner der Kategorien eine 100%-ige Punktzahl erzielen möchten. Ziel des Quiz ist es, Ihre Ansichten zu hinterfragen. Wenn Sie Anregungen oder konstruktive Kritik haben, füllen Sie bitte dieses <a href='https://forms.gle/6WZYMb5GXkkDLhxr5'>Kurzformular</a> aus oder öffnen Sie ein Problem auf der <a href='https://github.com/LeftValues/leftvalues.github.io'>GitHub-Seite</a>.",
         "ko": `어떠한 범주에서든 100%의 점수를 얻을 수 없다는 것을 기억하십시오. 이 설문의 목적은 당신 자신의 관점에 직면하는 것에 있습니다. 의견이나 건설적인 비판은 <a href="https://forms.gle/6WZYMb5GXkkDLhxr5">이 양식</a>이나 <a href="https://github.com/LeftValues/leftvalues.github.io">GitHub Page</a>의 이슈를 통해 개진할 수 있습니다.`,
         "ru": "Пожалуйста, помните, что вы не намерены получить 100%-ый результат ни в одной из осей. Смысл викторины заключается в том, чтобы оспорить ваши взгляды. Если у вас какие-либо предложения или конструктивная критика, то, пожалуйста, заполните эту <a href='https://forms.gle/6WZYMb5GXkkDLhxr5'>короткую форму</a> или откройте вкладку ''issue'' на <a href='https://github.com/LeftValues/leftvalues.github.io'>странице GitHub</a>.",
-        "cn": "请记住，你没必要在任何一类中获得100%的分数。测试的重点是挑战你的观点。如果你有任何建议或是建设性的批评，请填写<a href='https://forms.gle/6WZYMb5GXkkDLhxr5'>这份表格</a>或是在<a href='https://github.com/LeftValues/leftvalues.github.io'>该项目的GitHub Page</a>上开个issue。"
+        "cn": "请记住，你没必要在任何一类中获得100%的分数。测试的重点是挑战你的观点。如果你有任何建议或是建设性的批评，请填写<a href='https://forms.gle/6WZYMb5GXkkDLhxr5'>这份表格</a>或是在<a href='https://github.com/LeftValues/leftvalues.github.io'>该项目的GitHub Page</a>上开个issue。",
+		"sh": "Imajte na umu da cilj nije da dobijete 100% na bilo kojoj kategoriji. Smisao kviza je da izazove vaše mišljenje. Ako imate bilo kakve prijedloge ili konstruktivnu kritiku molimo vas da ispunite ovaj <a href='https://forms.gle/6WZYMb5GXkkDLhxr5'>kratki formular</a> ili otvorite pitanje na <a href='https://github.com/LeftValues/leftvalues.github.io'>GitHub stranici</a>."
     },
     "result-a-label": {
         "en": ["Extremely Revolutionary","Very Revolutionary","Revolutionary","Neutral","Reformist","Very Reformist","Extremely Reformist"],
         "de": ["Extrem Revolutionär", "Sehr Revolutionär", "Revolutionär", "Neutral", "Reformistisch", "Sehr Reformistisch", "Extrem Reformistisch"],
         "ko": ["극단적 혁명주의","강경한 혁명주의","혁명주의","중립","개혁주의","강경한 개혁주의","극단적 개혁주의"],
         "ru": ["Крайне Революционный","Очень Революционный","Революционный","Нейтральный","Реформистский","Очень Реформистский","Крайне Реформистский"],
-        "cn": ["极端革命派","非常革命派","革命派","中立","改良派","非常改良派","极端改良派"]
+        "cn": ["极端革命派","非常革命派","革命派","中立","改良派","非常改良派","极端改良派"],
+		"sh": ["Izuzetno Revolucionarni","Vrlo Revolucionarni","Revolucionarni","Neutralni","Reformistički","Vrlo Reformistički","Izuzetno Reformistički"]
     },
     "result-b-label": {
         "en": ["Extremely Scientific","Very Scientific","Scientific","Neutral","Utopian","Very Utopian","Extremely Utopian"],
         "de": ["Extrem Wissenschaftlich", "Sehr Wissenschaftlich", "Wissenschaftlich", "Neutral", "Utopisch", "Sehr Utopisch", "Extrem Utopisch"],
         "ko": ["극단적 과학주의","강경한 과학주의","과학주의","중립","공상주의","강경한 공상주의","극단적 공상주의"],
         "ru": ["Крайне Научный","Очень Научный","Научный","Нейтральный","Утопичный","Очень Утопичный","Крайне Утопичный"],
-        "cn": ["极端科学社会主义","非常科学社会主义","科学社会主义","中立","空想社会主义","非常空想社会主义","极端空想社会主义"]
+        "cn": ["极端科学社会主义","非常科学社会主义","科学社会主义","中立","空想社会主义","非常空想社会主义","极端空想社会主义"],
+		"sh": ["Izuzetno Naučni","Vrlo Naučni","Naučni","Neutralni","Utopijski","Vrlo Utopijski","Izuzetno Utopijski"]
     },
     "result-c-label": {
         "en": ["Extremely Centralist","Very Centralist","Centralist","Neutral","Decentralist","Very Decentralist","Extremely Decentralist"],
         "de": ["Extrem Zentralisiert", "Sehr Zentralisiert", "Zentralisiert", "Neutral", "Dezentralisiert", "Sehr Dezentralisiert", "Extrem Dezentralisiert"],
         "ko": ["극단적 중앙집권주의", "강경한 중앙집권주의", "중앙집권주의", "중립", "분권주의", "강경한 분권주의", "극단적 분권주의"],
         "ru": ["Крайне Централизованный","Очень Централизованный","Централизованный","Нейтральный","Децентрализованный","Очень Децентрализованный","Крайне Децентрализованный"],
-        "cn": ["极端集权","非常集权","集权","中立","分权","非常分权","极端分权"]
+        "cn": ["极端集权","非常集权","集权","中立","分权","非常分权","极端分权"],
+		"sh": ["Izuzetno Centralistički","Vrlo Centralistički","Centralistički","Neutralni","Decentralistički","Vrlo Decentralistički","Izuzetno Decentralistički"]
     },
     "result-d-label": {
         "en": ["Extremely Internationalist","Very Internationalist","Internationalist","Neutral","Nationalist","Very Nationalist","Extremely Nationalist"],
         "de": ["Extrem Internationalistisch", "Sehr Internationalistisch", "Internationalistisch", "Neutral", "Nationalistisch", "Sehr Nationalistisch", "Extrem Nationalistisch"],
         "ko": ["극단적 세계주의","강경한 세계주의","세계주의","중립","국가주의","강경한 국가주의","극단적 국가주의"],
         "ru": ["Крайне Интернациональный","Очень Интернациональный","Интернациональный","Нейтральный","Национальный","Очень Национальный","Крайне Национальный"],
-        "cn": ["极端国际主义","非常国际主义","国际主义","中立","民族主义","非常民族主义","极端民族主义"]
+        "cn": ["极端国际主义","非常国际主义","国际主义","中立","民族主义","非常民族主义","极端民族主义"],
+		"sh": ["Izuzetno Internacionalistički","Vrlo Internacionalistički","Internacionalistički","Neutralni","Nacionalistički","Vrlo Nacionalistički","Izuzetno Nacionalistički"]
     },
     "result-e-label": {
         "en": ["Extremely Partisan","Very Partisan","Partisan","Neutral","Unionist","Very Unionist","Extremely Unionist"],
         "de": ["Extrem Parteiisch", "Sehr Parteiisch", "Parteiisch", "Neutral", "Gewerkschaftlich", "Sehr Gewerkschaftlich", "Extrem Gewerkschaftlich"],
         "ko": ["극단적 정당주의", "강경한 정당주의", "정당주의", "중립", "조합주의", "강경한 조합주의", "극단적 조합주의"],
         "ru": ["Крайне Партийный","Очень Партийный","Партийный","Нейтральный","Профсоюзный","Очень Профсоюзный","Крайне Профсоюзный"],
-        "cn": ["极端倾向党派","非常倾向党派","倾向党派","中立","倾向工会","非常倾向工会","极端倾向工会"]
+        "cn": ["极端倾向党派","非常倾向党派","倾向党派","中立","倾向工会","非常倾向工会","极端倾向工会"],
+		"sh": ["Izuzetno Partijski","Vrlo Partijski","Partijski","Neutralni","Sindikatski","Vrlo Sindikatski","Izuzetno Sindikatski"]
     },
     "result-f-label": {
         "en": ["Extremely Productivist","Very Productivist","Productivist","Neutral","Ecological","Very Ecological","Extremely Ecological"],
         "de": ["Extrem Produktivist", "Sehr Produktivist", "Produktivist", "Neutral", "Ökologisch", "Sehr Ökologisch", "Extrem Ökologisch"],
         "ko": ["극단적 생산주의", "강경한 생산주의", "생산주의", "중립", "생태주의", "강경한 생태주의", "극단적 생태주의"],
         "ru": ["Крайне Производственный","Очень Производственный","Производственный","Нейтральный","Экологический","Очень Экологический","Крайне Экологический"],
-        "cn": ["极端生产主义","非常生产主义","生产主义","中立","生态主义","非常生态主义","极端生态主义"]
+        "cn": ["极端生产主义","非常生产主义","生产主义","中立","生态主义","非常生态主义","极端生态主义"],
+		"sh": ["Izuzetno Produktivistički","Vrlo Produktivistički","Produktivistički","Neutralni","Ekološki","Vrlo Ekološki","Izuzetno Ekološki"]
     },
     "result-g-label": {
         "en": ["Extremely Conservative","Very Conservative","Conservative","Neutral","Progressive","Very Progressive","Extremely Progressive"],
         "de": ["Extrem Konservativ", "Sehr Konservativ", "Konservativ", "Neutral", "Progressiv", "Sehr Progressiv", "Extrem Progressiv"],
         "ko": ["극단적 보수주의", "강경한 보수주의", "보수주의", "중립", "진보주의", "강경한 진보주의", "극단적 진보주의"],
         "ru": ["Крайне Консервативный", "Очень Консервативный", "Консервативный", "Нейтральный", "Прогрессивный", "Очень Прогрессивный", "Крайне Прогрессивный"],
-        "cn": ["极端保守主义","非常保守主义","保守主义","中立","进步主义","非常进步主义","极端进步主义"]
+        "cn": ["极端保守主义","非常保守主义","保守主义","中立","进步主义","非常进步主义","极端进步主义"],
+		"sh": ["Izuzetno Konzervativni","Vrlo Konzervativni","Konzervativni","Neutralni","Progresivni","Vrlo Progresivni","Izuzetno Progresivni"]
     },
     "result-back": {
         "en": "Back",
         "de": "Zurück",
         "ko": "뒤로",
         "ru": "Назад",
-        "cn": "返回"
+        "cn": "返回",
+		"sh": "Nazad"
     },
     // index.html
     "index-b-start": {
@@ -250,14 +281,16 @@ const i18n = {
         "de": "Klicke hier um anzufangen!",
         "ko": "클릭해서 시작하기!",
         "ru": "Начать тест",
-        "cn": "点击这里开始！"
+        "cn": "点击这里开始！",
+		"sh": "Kliknite ovdje da započnete test!"
     },
     "index-h2-whatis" : {
         "en": "What is LeftValues?",
         "de": "Was ist LeftValues?",
         "ko": "LeftValues가 무엇입니까?",
         "ru": "Что такое LeftValues?",
-        "cn": "什么是LeftValues？"
+        "cn": "什么是LeftValues？",
+		"sh": "Šta je LeftValues?"
     },
     "index-p-answer": {
         "en": "LeftValues is a leftist quiz inspired by and based upon the <a href='https://8values.github.io/'>8values</a> quiz that seeks to identify your position on the left-wing spectrum. " + 
@@ -276,21 +309,26 @@ const i18n = {
         "В данном тесте <b><u><span id='numOfQuestions'></span></u></b> вопросов.",
         "cn": "LeftValues是一个确定你在左翼光谱中的位置的左派测试，受<a href='https://8values.github.io/'>8values测试</a>启发并基于其制作。" +
         "如果你不是左派，这个测试明显不适合你。你会看到一系列观点，然后你需要回答你对这些观点的看法。从<b>强烈同意</b>到<b>强烈反对</b>，每个回答都会影响你的分数。关于每个坐标轴的问题将依次给出，而不是打乱的。测试结束时，你的答案会和每个价值观的最大可能值做比较，从而得出一个百分比。请诚实作答！<br/><br/>" +
-        "测试中一共有<b><u><span id='numOfQuestions'></span></u></b>个问题。"
+        "测试中一共有<b><u><span id='numOfQuestions'></span></u></b>个问题。",
+		"sh": "LeftValues je ljevičarski kviz inspirisan i zasnovan na <a href='https://8values.github.io/'>8values</a> kvizu koji identifikuje vap pološaj na ljevičarskom političkom spektru. " + 
+        "Ako niste ljevičar, ovaj kviz očigledno nije prikladan za vas. Biće vam postavljena pitanja, i onda ćete odgovoriti sa vašim mišljenjem o tom pitanju, od <b>Potpuno Se Slažem</b> do <b>Uopšte Se Ne Slažem</b>. Svaki odgovor utiče na vaše rezultate. Pitanja za svaku osu su postavljena u redu. Na kraju kviza, vaši odgovori će biti upoređeni sa maksimumom mogućim za svaku osu. Odgovarajte iskreno!<br/><br/>" +
+        "U testu su <b><u><span id='numOfQuestions'></span></u></b> pitanja."
     },
     "index-h2-whatvalues": {
         "en": "What are the values?",
         "de": "Was sind die Werte?",
         "ko": "가치(values)는 무엇입니까?",
         "ru": "За что отвечают значения?",
-        "cn": "这些价值观是什么？"
+        "cn": "这些价值观是什么？",
+		"sh": "Koje su vrijednosti u pitanju?"
     },
     "index-sixaxes": {
         "en": "There are currently seven axes, each of which has two opposing values. They are:",
         "de": "Derzeit gibt es sechs Achsen, von denen jede zwei entgegengesetzte Werte hat. Sie sind:",
         "ko": "현재 6개의 축이 있습니다. 각각의 축은 아래와 같은 상반되는 두 개의 가치를 갖게 됩니다.:",
         "ru": "Есть семь независимых осей, и каждая имеет две противоположные ценности, присвоенные им.",
-        "cn": "当前一共有七个坐标轴，其各自代表了一对相反的价值观。它们是："
+        "cn": "当前一共有七个坐标轴，其各自代表了一对相反的价值观。它们是：",
+		"sh": "Trenutno postoji sedam osa sa suprotstavljenim vrijednostima. To su:"
     },
     "index-rev-desc": {
         "en": "<b style='color:#890000;'>Revolution</b> <b>vs.</b> <b style='color:#FC5959;'>Reform</b><br/>" +
@@ -302,7 +340,9 @@ const i18n = {
         "ru": "<b style='color:#890000;'>Революция</b> <b>vs.</b> <b style='color:#FC5959;'>Реформы</b><br/>" +
         "Те, кто имеет более высокий балл Революции, склонны поддерживать радикальное и быстрое свержение капиталистической системы посредством массового восстания. Те, кто имеет более высокий балл Реформ, склонны выступать за постепенные изменения внутри капиталистических структур, таких как либеральная демократия, с конечной целью достижения социализма.",
         "cn": "<b style='color:#890000;'>革命</b> <b>vs.</b> <b style='color:#FC5959;'>改良</b><br/>" +
-        "革命得分较高的人倾向于支持通过大规模起义来激进和迅速地推翻资本主义制度。改革得分较高的人倾向于支持在资本主义结构（如自由民主制）中引导渐进的变化，最终目标是实现社会主义。"
+        "革命得分较高的人倾向于支持通过大规模起义来激进和迅速地推翻资本主义制度。改革得分较高的人倾向于支持在资本主义结构（如自由民主制）中引导渐进的变化，最终目标是实现社会主义。",
+		"sh": "<b style='color:#890000;'>Revolucija</b> <b>vs.</b> <b style='color:#FC5959;'>Reforma</b><br/>" +
+		 "Oni sa višom vrijednošću revolucije inače podržavaju brzo i radikalno svrgavanje kapitalističkog sistema putem masovnog ustanka. Oni sa višom vrijednošću reforme inače preferiraju uvođenje postepenih promjena u kapitalističke strukture, kao što je liberalna demokratija, sa eventualnim ciljem dostizanja socijalizma."
     },
     "index-sci-desc": {
         "en": "<b style='color:#88232B;'>Scientific</b> <b>vs.</b> <b style='color:#7F0037;'>Utopian</b><br/>" +
