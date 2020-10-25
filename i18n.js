99bd25611f2270d1d2a518a9f3dcade91a73ffc0
azerhjk <73423096+azerhjk@users.noreply.github.com>
2020-10-25 15:14:41 +0100

    French langPicker

--- i18n.js
+++ i18n.js
@@ -126,7 +126,8 @@ async function loadTranslation () { // eslint-disable-line no-unused-vars
     getl10n('pt'),
     getl10n('ru'),
     getl10n('sh'),
-    getl10n('pl')
+    getl10n('pl'),
+    getl10n('fr')
   ]).then(() => {
     console.log(i18n)
 
